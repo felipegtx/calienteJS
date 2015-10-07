57dc6e3666410d56d30c95624ac222ec9c826ed4
Felipe <felipe.g.te@gmail.com>
2015-10-07 18:57:18 -0300

    A fix for infinit growt on redundant postions

--- dist/caliente.js
+++ dist/caliente.js
@@ -437,16 +437,21 @@
             window.clearTimeout(_timeout);
         }
         if (_evt !== null) {
-            propagate(_evt.clientX, _evt.clientY);
-            if (_objCaliente.isType(_onMove, "function")) {
-                _onMove(_rawResult.slice());
+            if ((_lastPosition.x !== _evt.clientX) || (_lastPosition.y !== _evt.clientY)) {
+                propagate(_evt.clientX, _evt.clientY);
+                _lastPosition.x = _evt.clientX;
+                _lastPosition.y = _evt.clientY;
+                if (_objCaliente.isType(_onMove, "function")) {
+                    _onMove(_rawResult.slice());
+                }
             }
         }
-        window.setTimeout(track, 250);
+        window.setTimeout(track, 100);
     }
 
     var _rawResult = [],
         _evt = null,
+        _lastPosition = { x: 0, y: 0 },
         _domCaliente = w.domCaliente,
         _objCaliente = w.objCaliente,
         _onMove = null,
